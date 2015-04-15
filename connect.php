001855e1958396d8628bfa26794e44b2e314a3eb
Tristan Whitaker <tristanrileyw@gmail.com>
2015-04-15 10:49:50 -0700

    video 2-4

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+//why does local host have to be first?
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks')
+//$mysqli->connect_error then we want it to die and have this message
+if ($mysqli->connect_error) {
+	die('Connect Error (' . $mysqli->connect_errno . ')'
+		. $mysqli->connect_error);
+}
+
+
+?>
