e86e2f0b8abeb2ec7a3f91aa0c71c49bbf1ba79c
Tristan Whitaker <tristanrileyw@gmail.com>
2015-04-15 10:58:15 -0700

    video 5

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,14 @@
 <?php
 //why does local host have to be first?
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks')
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 //$mysqli->connect_error then we want it to die and have this message
 if ($mysqli->connect_error) {
 	die('Connect Error (' . $mysqli->connect_errno . ')'
 		. $mysqli->connect_error);
 }
-
+else{
+	echo "Connection made";
+}
+$mysqli->close();
 
 ?>
