87fce52ba9991010aab030f2396e586639bca4a7
Tristan Whitaker <tristanrileyw@gmail.com>
2015-04-16 11:27:54 -0700

    video 9

--- index.php
+++ index.php
@@ -16,4 +16,23 @@
 	</form>
 	</div>
 </body>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task();
+
+	function add_task () {
+		$('.add-new-task').submit(function()
+			var new_task = $('.add-new-task input[name=new-task]').val();
+
+			if (new_task != '') {
+				$.post('includes/add-task.php', {task: new_task}, function(data) {
+					$(('add-new-task input[name=new-task]').val();
+						$(data).appendTo('task-list ul').hide().fadeIn();
+						
+				});
+			}		
+				return false;
+			});
+	}
+</script>
 </html>
